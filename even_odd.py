b1dff3ca563f522b8f5d5a6ab4c22dec6c0419da
Kotlarakesh422 <kotlarakesh143@gmail.com>
2024-06-27 11:30:52 +0530

    pyhonprogram

---
+++ even_odd.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("num is Even".format(num))
+else:
+   print("num is Odd".format(num))
---
+++ palindrome.py
@@ -0,0 +1,6 @@
+n=input()
+rev=n[::-1]
+if(n==rev):
+    print("the given number is palindrome:",n)
+else:
+    print("the given number is not palindrome:",n)
